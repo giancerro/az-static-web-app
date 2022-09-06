9bf35f88b0637a46652f63502c7c88717bf0d8c6
Gianluca Cerratti <gianluca.cerratti@igt.com>
2022-09-06 14:18:26 +0200

    Prova da zero

--- api/CheckTicket/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('Send devices from Cosmos');
-  
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: context.bindings.devices
-    };
-    
-};
